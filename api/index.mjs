ec0e66b416b16518ea89a6468e1f9c4aff727073
adewale <obadunadewale1234@gmail.com>
2024-06-18 20:38:59 +0100

    implement comment section api

---
+++ api/index.mjs
@@ -0,0 +1,34 @@
+import 'dotenv/config';
+import express from 'express';
+import routes from "./routes/index.mjs";
+import mongoose from 'mongoose';
+import cors from "cors"
+
+const mongoUri = process.env.MONGO_URI;
+
+const app = express();
+
+// CORS middleware
+app.use(cors({
+    origin: 'http://localhost:3000' 
+}));
+  
+
+// @ts-ignore
+mongoose.connect(mongoUri)
+    .then(() => {
+        console.log('database sucessfully connected')
+    })
+    .catch((err) => {
+        console.log(`error: ${err}`)
+    })
+        
+app.use(express.json());
+app.use(routes);
+
+app.get('/', (request, response) => {
+    return response.status(200).send('hello from express on vercel')
+})
+
+
+export default app;
